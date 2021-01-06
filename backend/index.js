52d2d3f98de227919e5d41e0fd20df35ba075e85
unknown <seve.pozzo@orange.fr>
2021-01-06 14:03:16 -0800

    cleaned code

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,32 @@
 const express = require('express')
 const app = express()
+const database = require('./src/database.js');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-//you fetch the tasks from the database and set tasks from the front end
+//you fetch the tasks from the database - all from the Tweets database
 const tweets = [
-{app.get('/tweets', (req, res) => {
+app.get('/tweets', (req, res) => {
     const tweets = [];
-    database.query("SELECT * tweet, id, name FROM tweets;", (errors, result) => {
+    database.query("SELECT * FROM tweets;", (errors, result) => {
         if(errors) {
             return res.json({message: 'Something went wrong..'});
         }
-        tweets = result;
+       res.json({tweets: results}); 
     });
-    res.json(tweets);
-});
-    
-}
+})
 ]
-//return the data back to the client
-  res.json(tweets)
-});
 
 
-App.post (/tweet, {req, res} => {
+app.post ('/tweet', (req, res) => {
     let tweet = req.body.tweet;
 
     database.query(`INSERT INTO tweets (tweet) VALUES ('${tweet}');`);
+    if (error) {
+      throw new Error;
+    }
 
     res.json({message: "Task saved"});
 

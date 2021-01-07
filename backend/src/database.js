a4b48820c55d93008216cbcbaf2f975d471e9769
Severine Pozzo <57165556+SeverinePozzo@users.noreply.github.com>
2021-01-06 19:18:22 -0800

    Update database.js

---
+++ backend/src/database.js
@@ -0,0 +1,26 @@
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+
+
+   host: '127.0.0.1',
+   user: 'root',
+   password: 'root',
+   database: 'tweets'
+})
+
+connection.connect((errors) => {
+
+   if(errors) {
+       throw Error;
+       
+
+   }
+
+   console.log("DB Connected Successfully");
+
+});
+
+
+module.exports = connection;
